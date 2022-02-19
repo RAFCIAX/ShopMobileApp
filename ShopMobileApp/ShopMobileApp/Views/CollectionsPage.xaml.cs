026375e5a8adf34d7a3efcbcc5512a35db97365d
Rafał Boruszewski <rafal.boruszewski@onet.pl>
2022-02-19 13:10:31 +0100

    Explore & Collections Page Design

--- ShopMobileApp/ShopMobileApp/Views/CollectionsPage.xaml.cs
+++ ShopMobileApp/ShopMobileApp/Views/CollectionsPage.xaml.cs
@@ -15,6 +15,25 @@ namespace ShopMobileApp.Views
         public CollectionsPage()
         {
             InitializeComponent();
+            this.BindingContext = this;
         }
+
+        public List<Category> MyCollections { get => GetCollections(); }
+
+        private List<Category> GetCollections()
+        {
+            var colList = new List<Category>();
+            colList.Add(new Category { Image = "WIP.png", Title = "MEN'S WRISTWATCHES" });
+            colList.Add(new Category { Image = "WIPP.png", Title = "WOMEN'S MINI DRESSES" });
+            colList.Add(new Category { Image = "WIP.png", Title = "WOMEN'S COATS" });
+            return colList;
+        }
+    }
+
+    public class Category
+    {
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Caption { get; set; }
     }
 }
--- ShopMobileApp/ShopMobileApp/Views/ExplorePage.xaml.cs
+++ ShopMobileApp/ShopMobileApp/Views/ExplorePage.xaml.cs
@@ -15,6 +15,19 @@ namespace ShopMobileApp.Views
         public ExplorePage()
         {
             InitializeComponent();
+            this.BindingContext = this;
+        }
+
+        public List<Category> AllCategories { get => GetCategories(); }
+
+        private List<Category> GetCategories()
+        {
+            var catList = new List<Category>();
+            catList.Add(new Category { Image = "BluzaElpolakoMiejskiTy.jpg", Title = "SUMMER COLLECTION", Caption = "BEST DISCOUNT THIS SEASON" });
+            catList.Add(new Category { Image = "BluzaElpolakoMiejskiTy.jpg", Title = "WOMEN'S CLOTHING", Caption = "UP TO 50% OFF ON EVERY ITEM" });
+            catList.Add(new Category { Image = "BluzaElpolakoMiejskiTy.jpg", Title = "ELEGANT CLOTHINGS", Caption = "UNIQUE COLLECTIONS AND STYLES" });
+            return catList;
         }
     }
+
 }
