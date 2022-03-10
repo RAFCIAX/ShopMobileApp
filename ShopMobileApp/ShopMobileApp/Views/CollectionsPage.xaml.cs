b2aa8cdded883e4203acec1946de57157350a866
Rafał Boruszewski <rafal.boruszewski@onet.pl>
2022-03-10 09:11:57 +0100

    Banner update + TabBar update + Items List + Resources

--- ShopMobileApp/ShopMobileApp/Views/CollectionsPage.xaml.cs
+++ ShopMobileApp/ShopMobileApp/Views/CollectionsPage.xaml.cs
@@ -12,6 +12,8 @@ namespace ShopMobileApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CollectionsPage : ContentPage
     {
+        
+
         public CollectionsPage()
         {
             InitializeComponent();
@@ -23,9 +25,47 @@ namespace ShopMobileApp.Views
         private List<Category> GetCollections()
         {
             var colList = new List<Category>();
-            colList.Add(new Category { Image = "WIP.png", Title = "MEN'S WRISTWATCHES" });
-            colList.Add(new Category { Image = "WIPP.png", Title = "WOMEN'S MINI DRESSES" });
-            colList.Add(new Category { Image = "WIP.png", Title = "WOMEN'S COATS" });
+            colList.Add(new Category { Image = "MPakPrzod.jpg", Title = "BMW E36 Front M Bumper", Price = "120.00" });
+            colList.Add(new Category { Image = "PaseoLamp.jpg", Title = "Toyota Paseo Rear Lamp", Price = "49.99" });
+            colList.Add(new Category { Image = "R34Hood.jpg", Title = "Nissan Skyline R34 Hood", Price = "225.00" });
+            colList.Add(new Category { Image = "R34Intercooler.jpg", Title = "Nissan Skyline R34 Intercooler Kit", Price = "445.00" });
+            colList.Add(new Category { Image = "CastrolOil.jpg", Title = "Castrol Magnetic Oil 5W-40 ", Price = "64.99" });
+            colList.Add(new Category { Image = "HonitonTools.jpg", Title = "Honiton Tool Set", Price = "149.99" });
+            colList.Add(new Category { Image = "H7.jpg", Title = "H7 Light Bulb", Price = "7.99" });
+            colList.Add(new Category { Image = "JR11.jpg", Title = "JR11 Rims", Price = "125.00"});
+            colList.Add(new Category { Image = "Engine2JZSwap.jpg", Title = "2JZ Engine Swap", Price = "1100.00"});
+            colList.Add(new Category { Image = "HertzAudio.jpg", Title = "Hertz Car Audio Speakers", Price = "78.99"});
+            colList.Add(new Category { Image = "JR12.jpg", Title = "JR12 Rims", Price = "112.00"});
+            colList.Add(new Category { Image = "ShellOil.jpg", Title = "Shell Helix Ultra 5W-40 Engine Oil", Price = "45.00"});
+            colList.Add(new Category { Image = "Rollcage.jpg", Title = "BMW E36 Rollcage", Price = "139.99"});
+            colList.Add(new Category { Image = "JR3.jpg", Title = "JR3 Rims", Price = "99.99"});
+            colList.Add(new Category { Image = "TurboKit.png", Title = "RB26DETT Turbo Kit", Price = "215.00"});
+            colList.Add(new Category { Image = "JRX5.jpg", Title = "JRX5 Rims", Price = "119.99"});
+            colList.Add(new Category { Image = "M52B28Swap.jpg", Title = "BMW M52B28 Engine Swap", Price = "89.99"});
+            colList.Add(new Category { Image = "R34Suspension.jpg", Title = "Nissan Skyline R34 Suspension Kit", Price = "211.00"});
+            colList.Add(new Category { Image = "ValvolineOil.jpg", Title = "Valvoline Premium 10W-30 Engine Oil", Price = "68.99"});
+            colList.Add(new Category { Image = "MPakPrzod.jpg", Title = "BMW E36 Front M Bumper", Price = "120.00" });
+            colList.Add(new Category { Image = "PaseoLamp.jpg", Title = "Toyota Paseo Rear Lamp", Price = "49.99" });
+            colList.Add(new Category { Image = "R34Hood.jpg", Title = "Nissan Skyline R34 Hood", Price = "225.00" });
+            colList.Add(new Category { Image = "R34Intercooler.jpg", Title = "Nissan Skyline R34 Intercooler Kit", Price = "445.00" });
+            colList.Add(new Category { Image = "CastrolOil.jpg", Title = "Castrol Magnetic Oil 5W-40 ", Price = "64.99" });
+            colList.Add(new Category { Image = "HonitonTools.jpg", Title = "Honiton Tool Set", Price = "149.99" });
+            colList.Add(new Category { Image = "H7.jpg", Title = "H7 Light Bulb", Price = "7.99" });
+            colList.Add(new Category { Image = "JR11.jpg", Title = "JR11 Rims", Price = "125.00"});
+            colList.Add(new Category { Image = "Engine2JZSwap.jpg", Title = "2JZ Engine Swap", Price = "1100.00"});
+            colList.Add(new Category { Image = "HertzAudio.jpg", Title = "Hertz Car Audio Speakers", Price = "78.99"});
+            colList.Add(new Category { Image = "JR12.jpg", Title = "JR12 Rims", Price = "112.00"});
+            colList.Add(new Category { Image = "ShellOil.jpg", Title = "Shell Helix Ultra 5W-40 Engine Oil", Price = "45.00"});
+            colList.Add(new Category { Image = "Rollcage.jpg", Title = "BMW E36 Rollcage", Price = "139.99"});
+            colList.Add(new Category { Image = "JR3.jpg", Title = "JR3 Rims", Price = "99.99"});
+            colList.Add(new Category { Image = "TurboKit.png", Title = "RB26DETT Turbo Kit", Price = "215.00"});
+            colList.Add(new Category { Image = "JRX5.jpg", Title = "JRX5 Rims", Price = "119.99"});
+            colList.Add(new Category { Image = "M52B28Swap.jpg", Title = "BMW M52B28 Engine Swap", Price = "89.99"});
+            colList.Add(new Category { Image = "R34Suspension.jpg", Title = "Nissan Skyline R34 Suspension Kit", Price = "211.00"});
+            colList.Add(new Category { Image = "ValvolineOil.jpg", Title = "Valvoline Premium 10W-30 Engine Oil", Price = "68.99"});
+            //colList.Add(new Category { Image = "", Title = "", Price = ""});
+
+
             return colList;
         }
     }
@@ -35,5 +75,6 @@ namespace ShopMobileApp.Views
         public string Image { get; set; }
         public string Title { get; set; }
         public string Caption { get; set; }
+        public string Price { get; set; }
     }
 }
--- ShopMobileApp/ShopMobileApp/Views/ShopPage.xaml.cs
+++ ShopMobileApp/ShopMobileApp/Views/ShopPage.xaml.cs
@@ -31,26 +31,28 @@ namespace ShopMobileApp.Views
         private List<Banner> GetBanners()
         {
             var bannerList = new List<Banner>();
-            bannerList.Add(new Banner { Heading = "SUMMER COLLECTION", Message = "40% Discount", Caption = "BEST DISCOUNT THIS SEASON", Image = "WIPP.png" });
-            bannerList.Add(new Banner { Heading = "WOMEN'S CLOTHINGS", Message = "UP TO 50% OFF", Caption = "GET 50% OFF ON EVERY ITEM", Image = "WIP.png" });
-            bannerList.Add(new Banner { Heading = "ELEGANT COLLECTION", Message = "20% Discount", Caption = "UNIQUE COMBINATIONS OF ITEMS", Image = "WIPP.png" });
+            bannerList.Add(new Banner { Heading = "Cars", Message = "NEW MODELS", Caption = "WIDE RANGE OF ITEMS", Image = "Cars.jpg" });
+            bannerList.Add(new Banner { Heading = "Car Parts", Message = "UP TO 50% OFF", Caption = "FIND BEST OFFERS", Image = "CarParts.jpg" });
+            bannerList.Add(new Banner { Heading = "Services", Message = "Best Quality", Caption = "SCHEDULE A SERVICE RIGHT NOW", Image = "Services.jpg" });
             return bannerList;
         }
 
         private List<Product> GetCollections()
         {
             var trendList = new List<Product>();
-            trendList.Add(new Product { Image = "WIP.png", Name = "Floral Bag + Hat", Price = "123.50" });
-            trendList.Add(new Product { Image = "WIPP.png", Name = "Satchel Bag", Price = "49.99" });
-            trendList.Add(new Product { Image = "WIP.png", Name = "Leather Bag", Price = "40.99" });
+            trendList.Add(new Product { Image = "MPakPrzod.jpg", Name = "BMW E36 Front M Bumper", Price = "120.00" });
+            trendList.Add(new Product { Image = "PaseoLamp.jpg", Name = "Toyota Paseo Rear Lamp", Price = "49.99" });
+            trendList.Add(new Product { Image = "R34Hood.jpg", Name = "Nissan Skyline R34 Hood", Price = "225.00" });
+            trendList.Add(new Product { Image = "R34Intercooler.jpg", Name = "Nissan Skyline R34 Intercooler Kit", Price = "445.00" });
             return trendList;
         }
 
         private List<Product> GetTrends()
         {
             var colList = new List<Product>();
-            colList.Add(new Product { Image = "WIP.png", Name = "Beige Heeled Shoe", Price = "109.99" });
-            colList.Add(new Product { Image = "WIPP.png", Name = "Shoe + Addons", Price = "225.99" });
+            colList.Add(new Product { Image = "CastrolOil.jpg", Name = "Castrol Magnetic Oil 5W-40 ", Price = "64.99" });
+            colList.Add(new Product { Image = "HonitonTools.jpg", Name = "Honiton Tool Set", Price = "149.99" });
+            colList.Add(new Product { Image = "H7.jpg", Name = "H7 Light Bulb", Price = "7.99" });
             return colList;
         }
 
